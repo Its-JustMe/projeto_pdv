e3395ebe019577b3849b5c6070e3a2d6c7dba9ef
Arthur Santos <108244695+Its-JustMe@users.noreply.github.com>
2024-05-15 03:22:57 +0000

    .

--- scripts/index.js
+++ scripts/index.js
@@ -4,14 +4,8 @@ import { jsonRequest } from "./modules/request.js";
 
 (function () {
     document.addEventListener('DOMContentLoaded', function () {
-        const jsonData = jsonRequest();
-        console.log(jsonData)
-
-        const menuBtn = document.querySelector('i.fa-bars');
-        menuBtn.addEventListener('click', () => navbarHandler());
-
-        if (window.innerWidth <= 768) {
-            navbarHandler();
-        }
+        document.querySelectorAll('.fa-bars').forEach(btn => {
+            btn.addEventListener('click', navbarHandler);
+        });
     });
 })();
--- scripts/modules/interactions.js
+++ scripts/modules/interactions.js
@@ -2,21 +2,19 @@ export function navbarHandler() {
     const navbar = document.querySelector('.navbar');
     const root = document.documentElement;
 
-    // Alterna a classe 'shown' para indicar a visibilidade da navbar
-    navbar.classList.toggle('shown');
-
-    // Verifica se a navbar está visível ou não
-    const navbarIsShown = navbar.classList.contains('shown');
-
-    // Atualiza a largura da navbar apenas se ela estiver visível
-    if (navbarIsShown) {
-        root.style.setProperty('--navbar-width', '20vw');
-        root.style.setProperty('--main-section-width', '50vw');
-        root.style.setProperty('--header-width', '80vw');
-    } else {
-        // Caso contrário, define a largura como zero
+    if (navbar.classList.contains('shown') && window.innerWidth > 768) {
+        navbar.classList.remove('shown');
         root.style.setProperty('--navbar-width', '8.5vw');
         root.style.setProperty('--main-section-width', '60vw');
         root.style.setProperty('--header-width', '93vw');
+    } else if (!navbar.classList.contains('shown') && window.innerWidth > 768) {
+        navbar.classList.add('shown');
+        root.style.setProperty('--navbar-width', '20vw');
+        root.style.setProperty('--main-section-width', '50vw');
+        root.style.setProperty('--header-width', '80vw');
+    } else if (navbar.classList.contains('shown') && window.innerWidth < 768) {
+        navbar.classList.remove('shown');
+    } else if (!navbar.classList.contains('shown') && window.innerWidth < 768) {
+        navbar.classList.add('shown');
     }
 }
